21f9e85bc6aac0859526c95a82ff6f57bd19aacb
me <me@me>
2023-05-24 17:06:52 +0800

    change domain to fit Pandora

--- src/utils/background.ts
+++ src/utils/background.ts
@@ -1,5 +1,5 @@
 chrome.runtime.onInstalled.addListener(async () => {
     // Reload tabs to apply content scripts
-    const tabs = await chrome.tabs.query({ url: ['https://chat.openai.com/*'] })
+    const tabs = await chrome.tabs.query({ url: ['https://chat-shared1.zhile.io/*'] })
     tabs.forEach(({ id }) => chrome.tabs.reload(id))
 })
